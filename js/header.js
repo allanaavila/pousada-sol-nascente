b7ec137fe21a0cf8f52059e5881af9c6e3c336d6
Allana <allanac.avila@gmail.com>
2024-01-30 09:53:05 -0300

    criando button login

--- js/header.js
+++ js/header.js
@@ -2,15 +2,16 @@ const searchWrapper = document.querySelector(".search-container");
 const searchIcon = document.getElementById("search-icon");
 const closeIcon = document.getElementById("close-icon");
 const searchInput = document.getElementById('search-input');
+const loginContainer = document.getElementById('login-icon');
 
 searchIcon.addEventListener("click", () => {
     searchWrapper.classList.add("active");
-    closeIcon.style.display = "inline"; // Mostra o ícone de fechar
+    closeIcon.style.display = "inline";
 });
 
 closeIcon.addEventListener("click", () => {
     searchWrapper.classList.remove("active");
-    closeIcon.style.display = "none"; // Esconde o ícone de fechar
+    closeIcon.style.display = "none"; 
     limparCampo();
 });
 
@@ -26,14 +27,12 @@ function handleEnterKeyPress(event) {
 
 searchInput.addEventListener('keyup', handleEnterKeyPress);
 
-// Opcional: Adiciona um ouvinte de evento para clicar fora da área de pesquisa
 document.addEventListener('click', (event) => {
     if (!searchWrapper.contains(event.target)) {
         searchWrapper.classList.remove("active");
-        closeIcon.style.display = "none"; // Esconde o ícone de fechar
+        closeIcon.style.display = "none"; 
         limparCampo();
     }
 });
 
-// O código abaixo é opcional e apenas para limpar o campo após 2 segundos
 setTimeout(limparCampo, 2000);
