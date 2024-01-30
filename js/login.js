52e1a3b087746183d279d6b73c9f5876256d1f51
Allana <allanac.avila@gmail.com>
2024-01-30 20:49:14 -0300

    criando animacao css

--- js/login.js
+++ js/login.js
@@ -2,14 +2,14 @@
 
 const btnSignin = document.querySelector("#signin");
 const btnSigup = document.querySelector("#signup");
-
 const body = document.querySelector("body");
 
 btnSignin.addEventListener("click", function() {
-    body.className = "sing-in-js";
+    body.classList.remove("sing-up-js");
+    body.classList.add("sing-in-js");
 });
 
 btnSigup.addEventListener("click", function() {
-    body.className = "sing-up-js";
+    body.classList.remove("sing-in-js");
+    body.classList.add("sing-up-js");
 });
-
