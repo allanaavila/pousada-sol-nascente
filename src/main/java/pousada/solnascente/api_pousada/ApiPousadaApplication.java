725479e733cdbfc77dcb65b31a142e3305978735
Allana Avila <allanavila@gmail.com>
2025-05-08 10:09:06 -0300

    iniciando projeto

---
+++ src/main/java/pousada/solnascente/api_pousada/ApiPousadaApplication.java
@@ -0,0 +1,13 @@
+package pousada.solnascente.api_pousada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPousadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPousadaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pousada/solnascente/api_pousada/model/Cliente.java
@@ -0,0 +1,24 @@
+package pousada.solnascente.api_pousada.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private boolean ativo;
+}
