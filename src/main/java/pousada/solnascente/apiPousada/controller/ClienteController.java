97f3a7cc3fd0a0f50d036e392361294f96e0048a
Allana Avila <allanavila@gmail.com>
2025-05-09 18:27:25 -0300

    criando endpoints de cliente, atualizar, desativar e filtrar

--- src/main/java/pousada/solnascente/apiPousada/controller/ClienteController.java
+++ src/main/java/pousada/solnascente/apiPousada/controller/ClienteController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import pousada.solnascente.apiPousada.model.Cliente;
 import pousada.solnascente.apiPousada.service.ClienteService;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 
 @RestController
 @RequestMapping("/clientes")
@@ -20,14 +23,49 @@ public class ClienteController {
         this.clienteService = clienteService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listarTodosClientes() {
+        List<Cliente> clientes = clienteService.listarTodosClientes();
+        return new ResponseEntity<>(clientes, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarClientePorId(@PathVariable Long id) {
+        try {
+            Cliente cliente = clienteService.buscarClientePorId(id);
+            return new ResponseEntity<>(cliente, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
     @PostMapping
     public ResponseEntity<Cliente> cadastrarCliente(@Valid @RequestBody Cliente cliente) {
         Cliente clienteCadastrado = clienteService.cadastrarCliente(cliente);
         return new ResponseEntity<>(clienteCadastrado, HttpStatus.CREATED);
     }
 
-    @GetMapping("/ping")
-    public ResponseEntity<String> ping() {
-        return ResponseEntity.ok("API está online!");
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> alterarCliente(@PathVariable Long id, @Valid @RequestBody Cliente clienteAtualizado) {
+        try {
+            Cliente clienteAlterado = clienteService.alterarCliente(id, clienteAtualizado);
+            return new ResponseEntity<>(clienteAlterado, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> desativarCliente(@PathVariable Long id) {
+        try {
+            clienteService.desativarCliente(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
 }
