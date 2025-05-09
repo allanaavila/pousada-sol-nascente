c642048ccf871ab47e926f73f36afdf09e8f217b
Allana Avila <allanavila@gmail.com>
2025-05-09 18:01:45 -0300

    conexao com docker

--- src/test/java/pousada/solnascente/api_pousada/ApiPousadaApplicationTests.java
+++ src/test/java/pousada/solnascente/api_pousada/ApiPousadaApplicationTests.java
@@ -1,9 +1,13 @@
 package pousada.solnascente.api_pousada;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import pousada.solnascente.apiPousada.ApiPousadaApplication;
 
-@SpringBootTest
+
+@SpringBootTest(classes = ApiPousadaApplication.class)
+@EnableAutoConfiguration
 class ApiPousadaApplicationTests {
 
 	@Test
