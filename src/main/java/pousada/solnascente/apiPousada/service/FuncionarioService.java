b18e05d7e0e1949d43a5d7d06d958a627fa0aa15
Allana Avila <allanavila@gmail.com>
2025-05-21 15:00:00 -0300

    criando interface de funcionario com seus metodos

--- src/main/java/pousada/solnascente/apiPousada/service/FuncionarioService.java
+++ src/main/java/pousada/solnascente/apiPousada/service/FuncionarioService.java
@@ -1,4 +1,15 @@
 package pousada.solnascente.apiPousada.service;
 
+import pousada.solnascente.apiPousada.controller.dto.FuncionarioDTO;
+import pousada.solnascente.apiPousada.model.Funcionario;
+
+import java.util.List;
+
 public interface FuncionarioService {
+    Funcionario cadastrarFuncionario(FuncionarioDTO funcionarioDTO);
+    Funcionario buscarFuncionarioPorId(Long id);
+    Funcionario alterarFuncionario(Long id, FuncionarioDTO funcionarioAtualizado);
+    void desativarFuncionario(Long id);
+    void reativarFuncionario(Long id);
+    List<FuncionarioDTO> listarTodosFuncionarios();
 }
