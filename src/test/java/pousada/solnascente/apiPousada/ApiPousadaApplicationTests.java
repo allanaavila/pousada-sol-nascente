f5c5aed5dfc6c6b21da40de47fbf56fa6b8e4a4d
Allana Avila <allanavila@gmail.com>
2025-05-21 11:16:08 -0300

    conflitos resolvidos

--- src/test/java/pousada/solnascente/apiPousada/ApiPousadaApplicationTests.java
+++ src/test/java/pousada/solnascente/apiPousada/ApiPousadaApplicationTests.java
@@ -1,18 +1,11 @@
 package pousada.solnascente.apiPousada;
 
 import org.junit.jupiter.api.Test;
-<<<<<<< HEAD
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-=======
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
 @EnableAutoConfiguration
->>>>>>> feature/endpoint-cliente
 class ApiPousadaApplicationTests {
 
 	@Test
